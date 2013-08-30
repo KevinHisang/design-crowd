ce5bc412df32c2306a77d61ecdbfeae8d3879db0
KevinHsiang <tan355255@gmail.com>
2013-08-30 16:20:02 +0800

    callback loader

--- loader.js
+++ loader.js
@@ -1,6 +1,15 @@
-var loadJS = function(js_url) {
+var loadJS = function(js_url, callback) {
 	var script = document.createElement('script');
 	script.setAttribute('src', js_url);
+	var isloaded = false;
+	script.onload = function(){
+		isloaded = true;
+	}
+	script.onreadystatechange = function(){
+		if (typeof callback === 'function' && isloaded) {
+			callback();
+		};	
+	}
 	return document.getElementsByTagName('body')[0].appendChild(script);
 };
 
@@ -20,5 +29,7 @@ loadJS('http://code.jquery.com/ui/1.10.3/jquery-ui.js');
 loadJS('https://dl.dropboxusercontent.com/u/65282618/jQueryRotate.2.2.js');
 
 loadJS('https://dl.dropboxusercontent.com/u/65282618/parser.js');
-loadJS('https://dl.dropboxusercontent.com/u/65282618/editor.js');
-loadJS('https://dl.dropboxusercontent.com/u/65282618/save.js');
+loadJS('https://dl.dropboxusercontent.com/u/65282618/unredo.js', function(){
+	loadJS('https://dl.dropboxusercontent.com/u/65282618/editor.js');
+	loadJS('https://dl.dropboxusercontent.com/u/65282618/save.js');	
+});
